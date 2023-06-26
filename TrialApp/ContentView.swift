e986f7cc5d131990343cbdb0a3cfd2285f48af71
Ashwini Pai <ashwinixpai@gmail.com>
2023-06-26 23:30:08 +0530

    Initial Commit

---
+++ TrialApp/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  TrialApp
+//
+//  Created by Ashwini Pai on 23/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    var body: some View {
+        VStack {
+            Text(textTitle)
+            TextField("", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+                .padding()
+            Button("Submit Name") {
+                textTitle = "Welcome \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .accentColor(.red)
+            
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ TrialApp/TrialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialAppApp.swift
+//  TrialApp
+//
+//  Created by Ashwini Pai on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TrialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
